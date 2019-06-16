e2a447743fa0a7fb94c165347dba43456070ecdd
Francisco <franciscobarros754@gmail.com>
2019-06-16 00:13:34 -0300

    criado rota para likes e controller de likes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 const uploadconfig = require('./config/uploader');
 const express = require('express');
 const multer = require('multer');
@@ -6,7 +7,12 @@ const multer = require('multer');
 const routes = new express.Router();
 const upload = multer(uploadconfig);
 
+
+//Rota para cadastro e listagem de Posts
 routes.post('/posts', upload.single('image'), PostController.store);
 routes.get('/posts', PostController.index);
+
+//Rota para likes
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
