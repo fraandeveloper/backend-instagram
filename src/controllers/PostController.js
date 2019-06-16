ce9149d7590741b2a10d52d4648d25f2e7e4c1cf
Francisco <franciscobarros754@gmail.com>
2019-06-16 00:45:35 -0300

    criado rota para files image e tratado image para redução do tamanho

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,6 +1,9 @@
 //Regras de negocios
 
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
@@ -11,14 +14,28 @@ module.exports = {
 
     async store(req, res) {
         const { author, place, description, hashtags } = req.body;
-        const { fielname: image } = req.file;
+        const { filename: image } = req.file;
+
+        const [ name ] = image.split('.');
+        const filename = `${name}.jpg`;
+
+        //Reduzindo o tamanho da imge
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile( 
+                path.resolve(req.file.destination, 'resized', filename)
+            )
+        
+            //Deletando a image original
+            fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
             author,
             place,
             description,
             hashtags,
-            image
+            image: filename
         });
         return res.json(post);
     }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 mongoose.connect('mongodb+srv://development:ctifpi@cluster0-cdehv.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
-
+app.use('/file', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
 
 app.listen(8000);
